0aea859d7f21e34d926eb6e40db7498b5a98791a
devwenzel <jose.wenzel@codurance.com>
2020-04-20 09:21:54 -0400

    refactor tennis tests

--- test/game_test.clj
+++ test/game_test.clj
@@ -3,10 +3,7 @@
             [score-calculation :refer :all]))
 
 (deftest game-tests
-  (testing "will return 'love love' when given a 0 0 score"
-           (is (= "love love" (score [0 0]))))
-  (testing "will return 'love fifteen' when given a 0 1 score"
-           (is (= "love fifteen" (score [0 1]))))
-  (testing "will return 'fifteen fifteen' when given a 1 1 score"
-           (is (= "fifteen fifteen" (score [1 1]))))
-  )
+  (testing "tennis scores"
+           (is (= "love love" (score [0 0])))
+           (is (= "love fifteen" (score [0 1])))
+           (is (= "fifteen fifteen" (score [1 1])))))
