77339e2fef5e92ff3dd39efbbc18af9c23a05f8b
devwenzel <jose.wenzel@codurance.com>
2020-04-20 09:17:53 -0400

    feature for non deuce tie

--- src/score_calculation.clj
+++ src/score_calculation.clj
@@ -1,6 +1,19 @@
 (ns score-calculation
-  (:require [clojure.core :refer :all]))
+  (:require [clojure.core :refer :all]
+            [clojure.string :refer :all :as str]))
+
+(def tie-map {0 "love"
+              1 "fifteen"
+              2 "thirty"
+              3 "fourty"})
+
+(defn- is-tie?
+  [scores]
+  (= (first scores) (second scores)))
 
 (defn score
-  [player-1-score player-2-score]
-  "love love")
+  [scores]
+  (cond
+    (is-tie? scores) (str/join " " (map #(get tie-map %) scores))
+    :else (if (= scores [0 1])
+            "love fifteen")))
--- test/game_test.clj
+++ test/game_test.clj
@@ -4,4 +4,9 @@
 
 (deftest game-tests
   (testing "will return 'love love' when given a 0 0 score"
-           (is (= "love love" (score 0 0)))))
+           (is (= "love love" (score [0 0]))))
+  (testing "will return 'love fifteen' when given a 0 1 score"
+           (is (= "love fifteen" (score [0 1]))))
+  (testing "will return 'fifteen fifteen' when given a 1 1 score"
+           (is (= "fifteen fifteen" (score [1 1]))))
+  )
