76fc5c65d9973da9187fa0844d3072e2d255a2fb
devwenzel <jose.wenzel@codurance.com>
2020-04-20 10:16:16 -0400

    extra tests for validation

--- test/game_test.clj
+++ test/game_test.clj
@@ -7,4 +7,6 @@
            (is (= "love love" (score [0 0])))
            (is (= "love fifteen" (score [0 1])))
            (is (= "fifteen fifteen" (score [1 1])))
-           (is (= "invalid input" (score [-1 0])))))
+           (is (= "invalid input" (score [-1 0])))
+           (is (= "invalid input" (score [0 -1])))
+           (is (= "invalid input" (score [-2 -6])))))
